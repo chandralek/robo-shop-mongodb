bef5bc7c6392546ee104fe85ea60c64d8cedd557
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-02 06:49:32 +0530

    Updating robo-shop-mongodb module

--- run.sh
+++ run.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-if [ -z "${MONGO_DB_URL}" ] ;then
-echo "MONGO_DB_URL is not specified"
-exit 1
-fi
+set +x
 
-host ${MONGO_DB_URL}
-if [ $? -ne 0 ] ;then
-echo "Unable to resolve host name"
-fi
-
-while true ;
-do
-nc -i 10 -z ${MONGO_DB_URL} 27017
-if [ $? -ne 0 ] ;then
-    echo "Port not yet opened up"
-    sleep 30
-    continue
-fi
-mongo -host ${MONGO_DB_URL} < users.js
-mongo -host ${MONGO_DB_URL} < catalogue.js
-exit $?
-done
+ls
